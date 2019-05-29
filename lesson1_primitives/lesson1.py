38623c80d1214c19e4de4e5f9880f67ba1d8e006
Daniel Fuller <lasaldan@gmail.com>
2019-05-28 22:30:09 -0600

    Add functions, conditionals and loops

--- lesson1_primitives/lesson1.py
+++ lesson1_primitives/lesson1.py
@@ -1,4 +1,25 @@
-var age = 5
-var name = "Sean"
-var hourlyWage = 16.50
-var single = true
+age = 25
+name = input("What is your name? ")
+hourlyWage = 16.50
+single = True
+
+def generateProfile():
+    profile = "My name is " + name + ". My age is " + str(age)
+    return profile
+
+print( generateProfile() )
+
+if(age > 24):
+    print( "You are very old." )
+    print( "And you're cool.")
+
+elif(age > 20):
+    print( "You are old.")
+
+else:
+    print( "You are young." )
+
+counter = 0
+while(counter < 5):
+    print("  Python is cool.\n\n")
+    counter = counter + 1
